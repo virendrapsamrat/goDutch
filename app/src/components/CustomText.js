170a1cfa74782db3ec82b789f9f20bf460682043
Virendra Samrat <virendra.samrat@wovvtech.com>
2020-10-25 11:42:30 +0530

    add static desgin

---
+++ app/src/components/CustomText.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const CustomText = ({text}) => {
+  return (
+    // <View>
+    <Text>{text}</Text>
+    // </View>
+  );
+};
+
+export default CustomText;
+
+const styles = StyleSheet.create({});
